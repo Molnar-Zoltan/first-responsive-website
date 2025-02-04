93ad3e2817fb4521d7ea4a5d8c0cca39fc6cd8d6
Molnar-Zoltan <weperdev@gmail.com>
2025-02-04 21:33:15 +0100

    Add Copy function for the E-Mail

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,57 @@
-const openButton = document.getElementById('open-sidebar-button')
-const mobileNavbar = document.getElementById('mobile-navbar')
+const openButton = document.getElementById('open-sidebar-button');
+const mobileNavbar = document.getElementById('mobile-navbar');
+let emailText;
+
 
   
 function openSidebar() {
-    mobileNavbar.classList.add('show')
-    openButton.setAttribute('aria-expanded', 'true')
+    mobileNavbar.classList.add('show');
+    openButton.setAttribute('aria-expanded', 'true');
 }
   
 function closeSidebar() {
-    mobileNavbar.classList.remove('show')
-    openButton.setAttribute('aria-expanded', 'false')
+    mobileNavbar.classList.remove('show');
+    openButton.setAttribute('aria-expanded', 'false');
 }
 
-function loadContent(file) {
+async function loadContent(file) {
 
-    fetch(file)
-    .then(response => {
+    // fetch(file) // Old code
+    // .then(response => {
+    //     if (!response.ok) {
+    //         throw new Error('Network response was not ok');
+    //     }
+    //     return response.text();
+    // })
+    // .then(html => {
+    //     document.getElementById('content').innerHTML = html;
+    // })
+    // .catch(error => {
+    //     console.error('There was a problem with the fetch operation:', error);
+    //     document.getElementById('content').innerHTML = '<p>Error loading content.</p>';
+    // })
+
+    try {
+        const response = await fetch(file);
+
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error("Failed to load the page.");
         }
-        return response.text();
-    })
-    .then(html => {
+
+        const html = await response.text();
+
         document.getElementById('content').innerHTML = html;
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-        document.getElementById('content').innerHTML = '<p>Error loading content.</p>';
-    })
+
+        if (file.includes("contact")) {
+            emailText = document.querySelector(".emailText");
+            resizeEmailInput();
+        }
+
+    }
+    catch(error) {
+        console.error(error);
+        ocument.getElementById('content').innerHTML = `<p>${error}</p>`;
+    }
 
 }
 
@@ -39,9 +63,27 @@ function showCoprightText() {
 }
 
 
-window.onload = function() {
-    loadContent('/pages/home.html');
-    showCoprightText();
+function copyText() {
+
+    const textCopied = document.querySelector(".textCopied");
+    
+    emailText.select();
+    emailText.setSelectionRange(0, 99999); // For mobile devices
+    navigator.clipboard.writeText(emailText.value)
+        .then(() => textCopied.style.display = "flex")
+        .catch(error => console.error("Error copying text:", error));
+
+    
 }
 
+function resizeEmailInput() { // Resizes the E-Mail input field to match the text length
+    emailText.style.width = emailText.value.length - 1 + "ch"; 
+}
+
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    loadContent('/pages/home.html');
+    showCoprightText();
+});
 
